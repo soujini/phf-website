4da5849d07654fe7233dd69a40f024aa42d6956a
souj <soujanya.venkatesh@gmail.com>
2018-10-17 18:12:33 +0530

    Heroku Configuration

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/phf-website/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+app.set( 'port', ( process.env.PORT || 8080 ));
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
