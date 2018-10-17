7de1d6eef56701460dfa54f3d941dc0a65ed77a5
souj <soujanya.venkatesh@gmail.com>
2018-10-17 16:50:54 +0530

    Config for Deployment

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<phf-website>'));
+app.use(express.static(__dirname + '/dist/phf-website'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<phf-website>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/phf-website/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
